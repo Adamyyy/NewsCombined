87bf6e7707eb372e30001340e34ba78341a5d85a
admin <admin@admins-mac-mini.local>
2018-01-03 21:56:14 +0200

    First git test

---
+++ NewsCombined/NewsCombined/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NewsCombined
+//
+//  Created by admin on 03/01/2018.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var Testo : String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
